a8c9d5a44255d07feebf6b53fe5bf7bc63ff9dc3
Robert Austin <austin437@hotmail.com>
2021-03-19 07:19:07 +0000

    Added menu page

--- bundlez.php
+++ bundlez.php
@@ -13,11 +13,11 @@
  * @package           Bundlez
  *
  * @wordpress-plugin
- * Plugin Name:       WordPress Plugin Boilerplate
+ * Plugin Name:       Bundlez
  * Plugin URI:        http://example.com/bundlez-uri/
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Description:       Plugin to bundle MemberPress memberships from disparate sites
  * Version:           1.0.0
- * Author:            Your Name or Your Company
+ * Author:            Associated Educational Technologies
  * Author URI:        http://example.com/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
